5df42ce26739d0fa6f68762629aaaa1519f942ef
syifamawardini2-cyber <syifamawardini2@gmail.com>
2026-05-22 10:39:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,10 @@ st.title("🎈 aplikasi rendemen")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+import streamlit as st
+
+code = '''def hello():
+    print("Hello, Streamlit!")'''
+st.code(code, language="python")
+
+Copy
