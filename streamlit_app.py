1fd5cc87a26809d37e88faefa3c244fea94d89a8
syifamawardini2-cyber <syifamawardini2@gmail.com>
2026-05-23 10:51:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,3 +16,12 @@ option = st.selectbox(
 )
 
 st.write("You selected:", option)
+import pandas as pd
+import streamlit as st
+
+uploaded_files = st.file_uploader(
+    "Upload data", accept_multiple_files=True, type="csv"
+)
+for uploaded_file in uploaded_files:
+    df = pd.read_csv(uploaded_file)
+    st.write(df)
